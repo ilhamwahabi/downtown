337ce9856499d97f434268a33b7439154772659b
iwgx <wahabiputra@gmail.com>
2019-05-06 20:10:30 +0700

    Updates

---
+++ precache-manifest.b77c34a95f5146514cfbcd8bfb3b2419.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0982396577215ce05f29830d7192dbb",
+    "url": "/downtown/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/downtown/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "becef4ba1b106cd7049c",
+    "url": "/downtown/static/js/main.b16c0d8b.chunk.js"
+  },
+  {
+    "revision": "cffc228ccb741c25be8d",
+    "url": "/downtown/static/js/runtime~main.dc0c3d3a.js"
+  }
+]);
