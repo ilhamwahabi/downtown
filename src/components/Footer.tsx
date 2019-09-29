aeff124e7b29bb65b77cde9a2ce6f0ce80e10d26
Ilham Wahabi <wahabiputra@gmail.com>
2019-09-29 16:39:28 +0700

    fix: Footer - remove Antd usage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,42 @@
 import React from "react";
 import { StyleSheet, css } from "aphrodite";
-import { Layout, Icon } from "antd";
 
-const { Footer } = Layout;
+import { ReactComponent as Copyright } from "../svg/copyright.svg";
 
 export default () => {
   return (
-    <Footer className={css(styles.footer)}>
+    <footer className={css(styles.footer)}>
       <a
         href="https://github.com/iwgx/downtown"
         className={css(styles.link)}
         target="_blank"
         rel="noopener noreferrer"
       >
-        Downtown <Icon type="copyright" /> 2019 Ilham Wahabi
+        <span>Downtown</span>
+        <Copyright className={css(styles.copyrightSvgIcon)} />
+        <span>2019 Ilham Wahabi</span>
       </a>
-    </Footer>
+    </footer>
   );
 };
 
 const styles = StyleSheet.create({
   footer: {
     backgroundColor: "white",
-    textAlign: "center"
+    textAlign: "center",
+    padding: "24px 50px"
   },
   link: {
-    color: "inherit"
+    color: "inherit",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  copyrightSvgIcon: {
+    width: 15,
+    height: 15,
+    fill: "rgba(0, 0, 0, 0.75)",
+    margin: "0 2.5px",
+    paddingBottom: 1
   }
 });
