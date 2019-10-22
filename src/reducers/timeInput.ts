25f3e3b9699f338ec29aa60bfb9e3ccfb83c17f3
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-22 13:07:49 +0700

    ref: timeInput reducer - remove validation

--- src/reducers/timeInput.ts
+++ src/reducers/timeInput.ts
@@ -23,45 +23,21 @@ export interface ITimeAction {
 export const reducers = (state: ITimeState, action: ITimeAction) => {
   switch (action.type) {
     case ETimeActionType.CHANGE_HOUR_FIRST_DIGIT:
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 2)
-        updatedState.hour = `${action.payload}${updatedState.hour[1]}`;
-
-      return updatedState;
+      return { ...state, hour: `${action.payload}${state.hour[1]}` };
     case ETimeActionType.CHANGE_HOUR_SECOND_DIGIT: {
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 3)
-        updatedState.hour = `${updatedState.hour[0]}${action.payload}`;
-
-      return updatedState;
+      return { ...state, hour: `${state.hour[0]}${action.payload}` };
     }
     case ETimeActionType.CHANGE_MINUTE_FIRST_DIGIT: {
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 5)
-        updatedState.minute = `${action.payload}${updatedState.minute[1]}`;
-
-      return updatedState;
+      return { ...state, minute: `${action.payload}${state.minute[1]}` };
     }
     case ETimeActionType.CHANGE_MINUTE_SECOND_DIGIT: {
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 9)
-        updatedState.minute = `${updatedState.minute[0]}${action.payload}`;
-
-      return updatedState;
+      return { ...state, minute: `${state.minute[0]}${action.payload}` };
     }
     case ETimeActionType.CHANGE_SECOND_FIRST_DIGIT: {
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 5)
-        updatedState.second = `${action.payload}${updatedState.second[1]}`;
-
-      return updatedState;
+      return { ...state, second: `${action.payload}${state.second[1]}` };
     }
     case ETimeActionType.CHANGE_SECOND_SECOND_DIGIT: {
-      const updatedState = { ...state };
-      if (parseInt(action.payload) <= 9)
-        updatedState.second = `${updatedState.second[0]}${action.payload}`;
-
-      return updatedState;
+      return { ...state, second: `${state.second[0]}${action.payload}` };
     }
     default:
       return state;
