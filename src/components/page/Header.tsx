4f40471b6b412aa7ee343af75ef7b3132c81b9b5
Ilham Wahabi <wahabiputra@gmail.com>
2019-11-16 08:24:06 +0700

    feat: Header - add settings icon

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { StyleSheet, css } from "aphrodite";
 
 import { useContextReducer } from "../../context";
+
+import { ReactComponent as SettingsIcon } from "../../svg/settings.svg";
 
 const Header: React.FC = () => {
   const {
@@ -8,7 +11,32 @@ const Header: React.FC = () => {
   } = useContextReducer();
 
   if (stage === "input") return null;
-  return <header>header</header>;
+
+  return (
+    <header className={css(styles.header)}>
+      <div>
+        <SettingsIcon className={css(styles.icon)} />
+      </div>
+    </header>
+  );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: "25px 50px",
+    width: "100%",
+    position: "fixed",
+    boxSizing: "border-box"
+  },
+  icon: {
+    width: 25,
+    padding: 10,
+    border: "1px solid black",
+    borderRadius: "50%",
+    cursor: "pointer"
+  }
+});
 
 export default Header;
